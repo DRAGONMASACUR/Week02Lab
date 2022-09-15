f3cfe288b5578c0cd5280b6914cc97503ad5b4da
1drag <1drag@LAPTOP-QQK1CMUP>
2022-09-15 09:14:57 -0600

    Created Lab program

---
+++ src/week02lab/Week02Lab.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week02lab;
+
+/**
+ *
+ * @author 1drag
+ */
+public class Week02Lab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Hello();
+        
+    }
+    public static void Hello() {
+    System.out.print("Hello, World!");
+}
+    
+}
